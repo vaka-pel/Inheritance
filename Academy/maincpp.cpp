d69e5320d15139f062e8b36f8fcc962a1ca5b909
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-08-18 20:34:00 +0300

    info aliignment DONE

--- Academy/maincpp.cpp
+++ Academy/maincpp.cpp
@@ -11,10 +11,18 @@ using std::endl;
 
 class Human
 {
+	static const int TYPE_WIDTH = 12;
+	static const int NAME_WIDTH = 12;
+	static const int AGE_WIDTH = 5;
+	static int count; //объявление статической переменной
 	std::string last_name;
 	std::string first_name;
 	int age;
 public:
+	static int get_count()
+	{
+		return count;
+	}
 	const std::string& get_last_name()const
 	{
 		return last_name;
@@ -46,10 +54,12 @@ public:
 		set_last_name(last_name);
 		set_first_name(first_name);
 		set_age(age);
+		count++;
 		cout << "HConstructor:\t" << this << endl;
 	}
 	virtual ~Human()
 	{
+		count--;
 		cout << "HDestructor:\t" << this << endl;
 
 	}
@@ -57,10 +67,27 @@ public:
 	// Methods
 	virtual std::ostream& info(std::ostream& os)const // base class
 	{
-		return os << last_name << " " << first_name << " " << age << endl;
+		os.width(TYPE_WIDTH); // метод width(n) задает размер поля,в которое будет выведено значение
+		os << std::left;
+		os << std::string(strchr(typeid(*this).name(), ' ') + 1) + ":";
+
+		//strchr(const char* str, char symbol); в указанной строке находит указанный символ
+		// (symbol), и возвращает указатель на найденный символ, или nullptr, 
+		// если указанный символ отсутствует в указанной строке.
+
+		//return os << last_name << " " << first_name << " " << age;
+
+		os.width(NAME_WIDTH);
+		os << last_name;
+		os.width(NAME_WIDTH);
+		os << first_name;
+		os.width(AGE_WIDTH);
+		os << age;
+		return os;
 	}
 
 };
+int Human::count = 0; //инициализация статической переменной (относится к определению класса - Class difinition)
 
 std::ostream& operator<<(std::ostream& os, const Human& obj)
 {
@@ -71,10 +98,13 @@ std::ostream& operator<<(std::ostream& os, const Human& obj)
 #define STUDENT_GIVE_PARAMETERS speciality, group, rating, attendance
 class Student :public Human
 {
+	static const int SPECIALITY_WIDTH = 32;
+	static const int GROUP_WIDTH = 8;
+	static const int RAT_WIDTH = 8;
 	std::string speciality;
 	std::string group;
-	double rating;
-	double attendance;
+	double rating; //успеваемость
+	double attendance;//посещаемость
 public:
 	const std::string& get_speciality()const
 	{
@@ -127,7 +157,17 @@ public:
 	// Methods
 	std::ostream& info(std::ostream& os)const override
 	{
-		return Human::info(os) << " " << speciality << " " << group << " " << rating << " " << attendance;
+		//return Human::info(os) << " " << speciality << " " << group << " " << rating << " " << attendance;
+		Human::info(os);
+		os.width(SPECIALITY_WIDTH);
+		os << speciality;
+		os.width(GROUP_WIDTH);
+		os << group;
+		os.width(RAT_WIDTH);
+		os << rating;
+		os.width(RAT_WIDTH);
+		os << attendance;
+		return os;
 	}
 };
 
@@ -135,6 +175,8 @@ public:
 #define TEACHER_GIVE_PARAMETERS speciality, experience
 class Teacher :public Human
 {
+	static const int SPECIALITY_WIDTH = 32;
+	static const int EXPERIENCE_WIDTH = 5;
 	std::string speciality;
 	int experience;
 public:
@@ -170,7 +212,13 @@ public:
 	// Methods
 	std::ostream& info(std::ostream& os)const override
 	{
-		return Human::info(os) << " " << speciality << " " << experience;
+		//return Human::info(os) << " " << speciality << " " << experience;
+		Human::info(os);
+		os.width(SPECIALITY_WIDTH);
+		os << speciality;
+		os.width(EXPERIENCE_WIDTH);
+		os << experience;
+		return os;
 	}
 	
 };
@@ -260,6 +308,8 @@ void main()
 		fout << *group[i] << endl;
 		cout << delimiter << endl;
 	}
+	cout << "Количество объектов: " << group[0]->get_count() << endl;
+	cout << "Количество объектов: " << Human::get_count() << endl;
 	fout.close();
 	system("notepad group.txt");
 
