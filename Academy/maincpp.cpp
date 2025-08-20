ca844f49d80bc029317cf5a6c731b992e493244f
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-08-20 21:33:41 +0300

    Academy MissionComplete

--- Academy/maincpp.cpp
+++ Academy/maincpp.cpp
@@ -370,14 +370,14 @@ Human** Load(const std::string& filename, int& n)
 		cout << "Position " << fin.tellg() << endl; // метод tellg() возвращает текущую get  позицию курсора на чтение. -1 конец файла
 		//4. Загружаем объекты из файла
 
-		for(int i = 0; !fin.eof(); )
+		for(int i = 0; i < n; i++)
 		{
 			std::string buffer;
 			std::getline(fin, buffer, ':');
 			if (buffer.size() < 5)continue;
 			group[i] = HumanFactory(buffer);
 			fin >> *group[i];
-			i++;
+			//i++;
 		}
 
 	}
@@ -445,6 +445,7 @@ void main()
 	int n = 0;
 	Human** group = Load("group.txt", n);
 	Print(group, n);
+	Save(group, n, "group2.txt");
 	Clear(group, n);
 
 }
