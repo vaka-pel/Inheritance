ee3ec6f3a40610e1fbd2d36cdf6dfb61d6d8c2fa
Pelivan <Pelivan@DESKTOP-29KL5UV>
2025-08-31 23:06:44 +0300

    FunctionPointers DONE

--- AbstractGeometry/main.cpp
+++ AbstractGeometry/main.cpp
@@ -84,7 +84,6 @@ namespace Geometry
 
 		virtual double get_area()const = 0;
 		virtual double get_perimeter()const = 0;
-		virtual void draw()const
 		virtual void draw()const = 0;
 		virtual void draw(int width, int height, BOOL (__stdcall *DrawFunction)(HDC, int, int, int, int))const
 		{
@@ -104,7 +103,6 @@ namespace Geometry
 			SelectObject(hdc, hBrush);
 
 			//5) ѕосле того, как все необходимые инструменты созданы и выбраны, можно рисовать:
-			::DrawFunction(hdc, start_x, start_y, start_x + width, start_y + height);
 			(*DrawFunction)(hdc, start_x, start_y, start_x + width, start_y + height);
 
 			//6) hdc, hPen и hBrush занимают ресурсы, а ресурсы нужно освобождать:
@@ -202,29 +200,6 @@ namespace Geometry
 		}
 		void draw()const override
 		{
-			//1) ѕолучаем окно консоли:
-			HWND hwnd = GetConsoleWindow();
-
-			//2) ѕолучаем контекст устройства (DC - Device Context) дл€ окна консоли:
-			HDC hdc = GetDC(hwnd);	//DC - это то, на чем мы будем рисовать
-
-			//3) —оздадим инструменты, которыми мы будем рисовать:
-			HPEN hPen = CreatePen(PS_SOLID, 5, color);	// арандаш (Pen) рисует контур фигуры.
-			HBRUSH hBrush = CreateSolidBrush(color);	// исть (Brush) отсует заливку фигуры.
-
-			//4) ¬ыьерим созданные инструменты:
-			SelectObject(hdc, hPen);
-			SelectObject(hdc, hBrush);
-
-			//5) ѕосле того, как все необходимые инструменты созданы и выбраны, можно рисовать:
-			::Rectangle(hdc, start_x, start_y, start_x + width, start_y + height);
-
-			//6) hdc, hPen и hBrush занимают ресурсы, а ресурсы нужно освобождать:
-
-			DeleteObject(hBrush);
-			DeleteObject(hPen);
-
-			ReleaseDC(hwnd, hdc);
 			Shape::draw(width, height, ::Rectangle);
 		}
 		void info()const override
@@ -270,19 +245,6 @@ namespace Geometry
 		}
 		void draw()const override
 		{
-			HWND hwnd = GetConsoleWindow();
-			HDC hdc = GetDC(hwnd);
-			HPEN hPen = CreatePen(PS_SOLID, line_width, color);
-			HBRUSH hBrush = CreateSolidBrush(color);
-
-			SelectObject(hdc, hPen);
-			SelectObject(hdc, hBrush);
-
-			::Ellipse(hdc, start_x, start_y, start_x + get_diameter(), start_y + get_diameter());
-
-			DeleteObject(hBrush);
-			DeleteObject(hPen);
-			ReleaseDC(hwnd, hdc);
 			Shape::draw(get_diameter(), get_diameter(), ::Ellipse);
 		}
 	};
